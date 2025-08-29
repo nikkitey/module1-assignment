302d49aaed203eb69df5be3010a39d5c38ac59d3
nikkitey <ntey1@student.alamo.edu>
2025-08-29 09:08:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Console message requirement
+console.log("Hello there! Can you answer this: What is 5 + 7?");
+
+// document.write requirement
+document.write("<h2>This line is written using document.write!</h2>");
+document.write("<p>If you see this text, your JavaScript file is linked correctly. ✅</p>");
